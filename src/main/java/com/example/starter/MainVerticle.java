aab93a4caec670f2e9bec0adb7825069c9097b9b
M. Inyilis Sa'da <inyilispunya@gmail.com>
2021-07-09 21:39:14 +0700

    router

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,13 +1,27 @@
 package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response().end("Hello from Vert.x!");
-    }).listen(8888);
+    Router router = Router.router(vertx);
+
+    router.get("/api/v1/hello").handler(this::helloVertx);
+    router.get("/api/v1/hello/:name").handler(this::helloName);
+
+    vertx.createHttpServer().requestHandler(router).listen(8888);
+  }
+
+  void helloVertx(RoutingContext routingContext) {
+    routingContext.request().response().end("Hello from Vert.x!");
+  }
+
+  void helloName(RoutingContext routingContext) {
+    String name = routingContext.pathParam("name");
+    routingContext.request().response().end(String.format("Hello %s!", name));
   }
 }
