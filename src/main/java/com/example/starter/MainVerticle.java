c98e8588edbc3ef6e3aeb40dc626141315c33f83
M. Inyilis Sa'da <inyilispunya@gmail.com>
2021-07-10 11:59:29 +0700

    Chained Routes And Static Handlers

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -11,6 +11,7 @@ import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -24,9 +25,22 @@ public class MainVerticle extends AbstractVerticle {
     vertx.deployVerticle(new HelloVerticle());
     Router router = Router.router(vertx);
 
+    router.route().handler(ctx -> {
+      String authToken = ctx.request().getHeader("AUTH_TOKEN");
+      if (authToken != null && "mySuperSecretAuthToken".contentEquals(authToken)) {
+        ctx.next();
+      } else {
+        ctx.response().setStatusCode(401).setStatusMessage("Unauthorized").end("Token wrong");
+      }
+    });
     router.get("/api/v1/hello").handler(this::helloVertx);
     router.get("/api/v1/hello/:name").handler(this::helloName);
+    router.route().handler(StaticHandler.create("web").setIndexPage("index.html"));
 
+    doConfig(start, router);
+  }
+
+  void doConfig(Promise<Void> start, Router router) {
     ConfigStoreOptions defaultConfig = new ConfigStoreOptions()
       .setType("file")
       .setFormat("json")
